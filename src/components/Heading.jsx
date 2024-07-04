e5c4a07503467b8649cd24863c8005defe49e64c
Daliya <daliyadeutsch@gmail.com>
2024-07-04 19:15:48 +0530

    changed title

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -5,9 +5,7 @@ function Heading({ bg }) {
     <>
       <h1 className="mb-4 font-extrabold leading-none tracking-tight md:text-5xl text-5xl text-white">
         Weather{" "}
-        <mark className={`px-2 text-white rounded bg-blue-500 ${bg}`}>
-          Application
-        </mark>
+        <mark className={`px-2 text-white rounded bg-blue-500 ${bg}`}>App</mark>
       </h1>
     </>
   );
